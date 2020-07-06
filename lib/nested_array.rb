49a7df711284c763aa375e2cf6e0fd8a3614028f
Israel Canessa <israellcanessa@gmail.com>
2020-07-06 16:50:06 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -40,6 +40,7 @@ def matrix_lookup(matrix, row, column)
     ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"]
   ]
     produce_storage[1][1]
+    produce_storage[1][3]
   # Given any matrix (array of arrays), a row index and a column index,
   # Return the matrix's content at that row and and column
 end
