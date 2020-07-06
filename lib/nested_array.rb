07ebf3b84b92a18a2fcdb6d38360e3aee8615e81
Israel Canessa <israellcanessa@gmail.com>
2020-07-06 16:43:17 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # Examples inspired by U. S. National Organic Standards
 
 ORGANIC_PRODUCE = [
@@ -34,6 +35,7 @@ def sorted_matrix
 end
 
 def matrix_lookup(matrix, row, column)
+
   # Given any matrix (array of arrays), a row index and a column index,
   # Return the matrix's content at that row and and column
 end
